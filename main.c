2086b64072d220018d12fc25eb2a07ac7e6795f3
corentin5002 <csabier@orange.fr>
2021-12-28 23:21:11 +0100

    prettyPrint & initialisation damier

--- main.c
+++ main.c
@@ -7,19 +7,79 @@
 #define J1 1
 #define J2 2
 #define CASE 0
+#define TRUE  1
+#define FALSE 0
+
 //==============================================================================
-//structure d'une case du damier------------------------------------------------
-typedef struct level level;
-struct level
+//structure d'une case du Damier------------------------------------------------
+typedef struct Case Case;
+struct Case
 {
 	int dame  ;
-	int joueur;
+	int equipe;
 };
 //------------------------------------------------------------------------------
+Case * genJeu()
+{
+    Case * Damier = malloc(50*sizeof(Case));
+    int i;
+    //initialisation du Damier
+    printf("| ");
+    for(i=0;i<50;i++){
+        if (i < 20)
+        {
+            Damier[i].equipe= J2;
+        }
+        else if (i > 29)
+        {
+            Damier[i].equipe= J1;
+        }
+        else Damier[i].equipe= CASE;
+        Damier[i].dame  = FALSE;
+        printf("%d ",Damier[i].equipe);
+        if (i%5 == 4) printf("|\n| ");
+    }
+    printf("\b\b \n");
+    return Damier;
+}
+//------------------------------------------------------------------------------
+Case *bougerPiece(Case* Damier,int cDepart,int cArrivee)
+{
+    Damier[cArrivee].equipe = Damier[cDepart].equipe;
+    Damier[cDepart].equipe = 0;
+    return Damier;
+}
+//------------------------------------------------------------------------------
+void prettyPrint(Case* Damier)
+{
+    printf("| ");
+    int i;
+    for(i=0;i<50;i++)
+    {
+        printf("%d ",Damier[i].equipe);
+        if (i%5 == 4) printf("|\n| ");
+    }
+    printf("\b\b \n");
+}
+//------------------------------------------------------------------------------
+void prettyPrint(Case* Damier)
+{
+    printf("| ");
+    int i;
+    for(i=0;i<50;i++)
+    {
+        printf("%d ",i);
+        if (i < 10)   printf(" ");
+        if (i%5 == 4) printf("|\n| ");
+    }
+    printf("\b\b \n");
+}
+//------------------------------------------------------------------------------
+
 int main()
 {
-
-    printf("%d\n", J1);
-
+    Case *Damier = genJeu();
+    Damier = bougerPiece(Damier,17,22);
+    prettyPrint(Damier);
     return 0;
 }
