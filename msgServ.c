ba3f7c50ab708b265d7d8ec956beecf1a4c03293
corentin5002 <csabier@orange.fr>
2022-01-06 19:35:07 +0100

    functions manipulation de fichier

--- msgServ.c
+++ msgServ.c
@@ -393,19 +393,85 @@ char* triageArrivee(char* msgRecu)
 		return msgRetour;
 	}
 }
-
-char* creaGuest(int* adrNbrGuest)
+//Ajout d'une chaine de caractère à la fin du fichier.
+void ajoutCompte(FILE* fichier,char* chaine)
 {
-	//incremente globalement le numero du guest
-	*adrNbrGuest += 1;
-	char* idGuest= malloc(sizeof(char));
-	strcat(idGuest,"Guest");
-	char* num = malloc(sizeof(char));
-	sprintf(num,"%d",*adrNbrGuest);
-	strcat(idGuest,num);
-	free(num);
-	return idGuest;
+	fseek(fichier, 0, SEEK_END);
+	fputc('-',fichier);
+	fputs(chaine,fichier);
+	fputs("\n",fichier);
+	fseek(fichier, 0, SEEK_SET);
 }
+
+void decoReco(FILE* fichier,long curseur,char nvChar)
+{
+	fseek(fichier,curseur,SEEK_SET);
+	fputc(nvChar,fichier);
+	fseek(fichier, 0, SEEK_SET);
+}
+
+int utiliserCompte(char* compte,int mode)
+{
+	FILE* fichier = fopen("comptes","r+");
+	char* buff = malloc(22*sizeof(char));
+	char* modifCompte=malloc(22*sizeof(char));
+	strcat(modifCompte,"-");
+	strcat(modifCompte,compte);
+	//Trouver un pseudo existant dans le fichier
+	if(mode == 0)
+	{
+		while(fscanf(fichier,"%s\n",buff) != -1)
+		{
+			//Si on détecte le nom dans la base de donnée
+			if(buff[0]=='-' && !strcmp(modifCompte,buff))
+			{
+				fseek(fichier,-strlen(compte)-2,SEEK_CUR);
+				decoReco(fichier,ftell(fichier),'+');
+				free(fichier);
+				return 1;
+			}
+		}
+	}
+	//Créer un nouveau compte n'existant pas dans le fichier
+	else if(mode == 1)
+	{
+		int test = 1;
+		char* enCours= malloc(sizeof(char));
+		strcat(enCours,"*");
+		strcat(enCours,compte);
+		while(fscanf(fichier,"%s\n",buff) != -1)
+		{
+
+			if(!strcmp(compte,buff))
+			{
+				//Si on détecte le nom de compte dans la BD
+				test = 0;
+				break;
+			}
+			else if(!strcmp(enCours,buff))
+			{
+				//Si on détecte le nom de compte dans la BD et qu'il est déjà connecté
+				test = 0;
+				break;
+			}
+		}
+		//Si le nom de compte n'a pas été trouvé
+		if(test)
+		{
+			printf("DEBUG %s\n",compte);
+			ajoutCompte(fichier,compte);
+			free(fichier);
+			return 1;
+		}
+	}
+	else
+	{
+		fprintf(stderr,"Erreur: mode invalide pour fonction utiliserCompte\n");
+	}
+	free(fichier);
+	return 0;
+}
+
 int main()
 {
 	system("clear");
